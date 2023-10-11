83bfef9bce01753f752e1dbee3dfae506c0319b5
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2023-10-11 13:19:03 -0500

    se crea las clases Repository de los 2 Entity, como intemediario entre capa aplicacion y negocio

---
+++ src/main/java/com/aplication/rest/repository/MakerRepository.java
@@ -0,0 +1,20 @@
+package com.aplication.rest.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aplication.rest.entities.Maker;
+
+@Repository
+public interface MakerRepository extends CrudRepository<Maker, Long>{
+    
+}
+
+/*
+1) creamos los repositorios para cada clase/entity
+ - estas clases van a ser interfaces que heredan de CrudRepository.
+   CrudRepository va a recibir <nom_Entity, tipo de dato de la PK de dicha clase> 
+   @Repository: puede actuar como una interfaz entre la capa de persistencia y la capa de negocio, y puede implementar operaciones básicas de CRUD
+
+
+*/
